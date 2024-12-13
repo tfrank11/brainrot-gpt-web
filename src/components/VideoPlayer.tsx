528cc4b450fe796536a378ccc89c8d8315bb8a6a
Timothy Frank <timmyfrank3@gmail.com>
2024-12-13 08:47:52 -0500

    download button

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,8 +1,9 @@
 import { useUser } from "@/hooks/useUser";
 import { createClient } from "@/lib/supabase/client";
-import { Button, Video } from "@react95/core";
+import { Button, Modal, Video } from "@react95/core";
 import React, { useEffect, useState } from "react";
 import { useAlertContext } from "./AlertProvider";
+import { Appwiz1502 } from "@react95/icons";
 
 interface Props {
   videoId: string;
@@ -38,30 +39,60 @@ const VideoPlayer: React.FC<Props> = ({ videoId, onRestart }) => {
   }, [user?.id, videoId, alert]);
 
   return (
-    <div className="flex flex-col gap-2">
-      {src && (
-        <Video
-          name="Video Player"
-          w="320px"
-          className="text-xs"
-          videoProps={{
-            src,
-          }}
-          src={src}
-        />
-      )}
-      <div className="flex gap-2 justify-around">
-        {onRestart && (
-          <Button
-            onClick={() => {
-              onRestart();
-            }}
-          >
-            Start Over
-          </Button>
-        )}
-      </div>
-    </div>
+    // @ts-expect-error fck this error
+    <Modal
+      icon={<Appwiz1502 variant="32x32_4" />}
+      className="w-fit m-auto"
+      type="info"
+      title="Brainrot GPT Wizard"
+      style={{
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <Modal.Content>
+        <div className="flex flex-col gap-2">
+          {src && (
+            <Video
+              name="Video Player"
+              w="320px"
+              className="text-xs"
+              videoProps={{
+                src,
+              }}
+              src={src}
+            />
+          )}
+          <div className="flex gap-2 justify-around">
+            {onRestart && (
+              <Button
+                onClick={() => {
+                  onRestart();
+                }}
+              >
+                Start Over
+              </Button>
+            )}
+            {src && (
+              <Button
+                onClick={() => {
+                  const link = document.createElement("a");
+                  link.target = "_blank";
+                  link.href = src;
+                  link.download = `${videoId}.mp4`;
+                  document.body.appendChild(link);
+                  link.click();
+                  document.body.removeChild(link);
+                }}
+              >
+                Download
+              </Button>
+            )}
+          </div>
+        </div>
+      </Modal.Content>
+    </Modal>
   );
 };
 
